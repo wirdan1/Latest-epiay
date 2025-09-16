f59642249761756814cc1540f794f09dabad8c5a
Danz-dev <danz124c@gmail.com>
2025-09-16 22:25:50 +0700

    Upload file: api/canvas/wlc.js

---
+++ api/canvas/wlc.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+module.exports = {
+  name: "Welcome",
+  desc: "Generate kartu sambutan",
+  category: "Canvas",
+  params: ["background", "description", "member", "avatar"],
+  async run(req, res) {
+    const {
+      background,
+      description,
+      member,
+      avatar
+    } = req.query;
+    const endpoint = req.path.split("/").pop().toLowerCase();
+    const fixedText1 = {
+      welcome: "WELCOME",
+      goodbye: "GOODBYE",
+      promote: "PROMOTE",
+      demote: "DEMOTE"
+    } [endpoint];
+    if (!background || !description || !member || !avatar) return res.status(400).json({
+      status: false,
+      error: "Parameter background, description, member, avatar wajib diisi!"
+    });
+    const url = `https://api.popcat.xyz/v2/welcomecard?background=${encodeURIComponent(background)}&text1=${encodeURIComponent(fixedText1)}&text2=${encodeURIComponent(description)}&text3=${encodeURIComponent(member)}&avatar=${encodeURIComponent(avatar)}`;
+    try {
+      const result = await axios.get(url, {
+        responseType: "arraybuffer"
+      });
+      res.setHeader("Content-Type", result.headers["content-type"]);
+      res.end(result.data);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        status: false,
+        error: err.message
+      });
+    }
+  }
+};
