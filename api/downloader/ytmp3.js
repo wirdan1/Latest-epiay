ffc53c396ac032de0ec43ab2b3d0f82f901343f3
Danz-dev <danz124c@gmail.com>
2025-09-16 22:26:17 +0700

    Upload file: api/downloader/ytmp3.js

---
+++ api/downloader/ytmp3.js
@@ -0,0 +1,27 @@
+const savetube = require("../../lib/savetube");
+module.exports = {
+  name: "Youtube Audio",
+  desc: "Download YouTube audio using SaveTube",
+  category: "Downloader",
+  params: ["url", "format"],
+  async run(req, res) {
+    const {
+      url,
+      format
+    } = req.query;
+    if (!url || !format) return res.status(400).json({
+      status: false,
+      error: "url & format is required"
+    });
+    try {
+      const result = await savetube.download(url, format);
+      if (!result.status) return res.status(result.code).json(result);
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(500).json({
+        status: false,
+        error: err.message
+      });
+    }
+  }
+};
