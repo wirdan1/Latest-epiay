bc1794eabe2ee423483acf42c9ab115829e3aec5
Danz-dev <danz124c@gmail.com>
2025-09-16 22:27:17 +0700

    Upload file: api/sfw/highfive.js

---
+++ api/sfw/highfive.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const path = require("path");
+module.exports = {
+  name: "Highfive",
+  desc: "Random highfive from waifu.pics",
+  category: "SFW",
+  async run(req, res) {
+    try {
+      const {
+        data
+      } = await axios.get("https://api.waifu.pics/sfw/highfive");
+      const ext = path.extname(data.url).toLowerCase();
+      const file = await axios.get(data.url, {
+        responseType: "arraybuffer"
+      });
+      const contentType = {
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+        ".jpeg": "image/jpeg",
+        ".gif": "image/gif",
+        ".mp4": "video/mp4"
+      } [ext] || "application/octet-stream";
+      res.setHeader("Content-Type", contentType);
+      res.send(file.data);
+    } catch (err) {
+      res.status(500).json({
+        status: false,
+        error: err.message
+      });
+    }
+  }
+};
