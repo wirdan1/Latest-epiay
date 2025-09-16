410a20d1b57b37be2a83d33ac3316399708b3563
Danz-dev <danz124c@gmail.com>
2025-09-16 22:28:20 +0700

    Upload file: settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: {
+    main: "Wudysoft API",
+    copyright: "Wudysoft API"
+  },
+  description: "Integrated API solution for your modern application development needs. Fast, secure, and reliable access.",
+  icon: "/image/icon.png",
+  author: "Wudysoft",
+  info_url: "https://whatsapp.com/channel/0029Vb0v3F71yT264EejzJ3e",
+  links: [{
+    name: "Channel WhatsApp Inf.",
+    url: "https://whatsapp.com/channel/0029Vb0v3F71yT264EejzJ3e"
+  }]
+};
