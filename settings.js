347e630100a40d17f33081a46f3203094f95672a
Danz-dev <danz124c@gmail.com>
2025-09-22 11:01:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ module.exports = {
     main: "Hookrest API",
     copyright: "Hookrest Team"
   },
-  description: "Integrated API solution for your modern application development needs. Fast, secure, and reliable access.",
+  description: "Integrated API solution for your modern application development needs. Fast, secure, free and reliable access.",
   icon: "/image/icon.png",
   author: "Hookrest-Team",
   info_url: "https://whatsapp.com/channel/0029Vb5CxIfAjPXInV7XWz38",
